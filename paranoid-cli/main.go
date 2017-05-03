a1c276f3f45f40950ecc1a79d3385d89c17242f3
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2017-05-03 17:16:01 +0000

    Ran go fmt

--- paranoid-cli/main.go
+++ paranoid-cli/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/urfave/cli"
 	pfscommands "github.com/pp2p/paranoid/libpfs/commands"
 	"github.com/pp2p/paranoid/logger"
 	"github.com/pp2p/paranoid/paranoid-cli/commands"
 	"github.com/pp2p/paranoid/paranoid-cli/tls"
+	"github.com/urfave/cli"
 	"os"
 	"os/user"
 	"path"
